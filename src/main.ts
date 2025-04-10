4880508c5afb032ad24df03e56fb2b86bec14fac
Wanderson Silva <wandersonsilva768@gmail.com>
2025-04-09 21:32:07 -0300

    feat: adiciona plugin de configuração do Pinia

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import App from '@/App.vue'
 
 import '@/plugins/yup'
 import '@/plugins/axios'
+import pinia from '@/plugins/pinia'
 import vuetify from '@/plugins/vuetify'
 
 import '@/scss/style.scss'
@@ -12,6 +13,7 @@ import '@/scss/style.scss'
 const app = createApp(App)
 
 app
+  .use(pinia)
   .use(router)
   .use(vuetify)
   .mount('#app')
