0716b94b592ef534a284deb6325c5ab91c7e4293
Wanderson Silva <wandersonsilva768@gmail.com>
2025-04-10 23:11:52 -0300

    feat: usuário será redirecionado para a página de login sempre que não estiver logado

--- src/router/MainRoutes.ts
+++ src/router/MainRoutes.ts
@@ -1,14 +1,29 @@
-export default {
+import type { RouteRecordRaw } from 'vue-router'
+
+import { useMeStore } from '@/stores/me'
+
+export default <RouteRecordRaw>({
   path: '/',
   meta: {
     requiresAuth: true
   },
+  beforeEnter: (to, from, next) => {
+    const meStore = useMeStore()
+
+    if (!meStore.isLoggedIn) {
+      return next({
+        name: 'login',
+      })
+    }
+
+    return next()
+  },
   component: () => import('@/layouts/full/FullLayout.vue'),
   children: [
     {
-      name: 'dashboard',
       path: '',
+      name: 'dashboard',
       component: () => import('@/views/dashboard/index.vue')
     }
   ]
-}
+})
