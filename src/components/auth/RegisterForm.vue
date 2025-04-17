592d6276e30be10d66dda0f812a096cb70edcd87
Wanderson Silva <wandersonsilva768@gmail.com>
2025-04-16 22:44:13 -0300

    refactor: aplica refatoração ao código do componente RegisterForm

--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -1,24 +1,63 @@
-<script setup lang="ts">
-import { ref } from 'vue';
-const checkbox = ref(true);
-</script>
-
 <template>
-    <v-row class="d-flex mb-3">
-        <v-col cols="12">
-            <v-label class="font-weight-bold mb-1">Name</v-label>
-            <v-text-field variant="outlined" hide-details color="primary"></v-text-field>
-        </v-col>
-        <v-col cols="12">
-            <v-label class="font-weight-bold mb-1">Email Address</v-label>
-            <v-text-field variant="outlined" type="email" hide-details color="primary"></v-text-field>
-        </v-col>
-        <v-col cols="12">
-            <v-label class="font-weight-bold mb-1">Password</v-label>
-            <v-text-field variant="outlined" type="password"  hide-details color="primary"></v-text-field>
-        </v-col>
-        <v-col cols="12" >
-            <v-btn to="/" color="primary" size="large" block   flat>Sign up</v-btn>
-        </v-col>
-    </v-row>
+  <v-row class="d-flex mb-3">
+    <v-col cols="12">
+      <v-label class="font-weight-bold mb-1">
+        Name
+      </v-label>
+
+      <v-text-field
+        hide-details
+        color="primary"
+        variant="outlined"
+      />
+    </v-col>
+
+    <v-col cols="12">
+      <v-label class="font-weight-bold mb-1">
+        Email Address
+      </v-label>
+
+      <v-text-field
+        type="email"
+        hide-details
+        color="primary"
+        variant="outlined"
+      />
+    </v-col>
+
+    <v-col cols="12">
+      <v-label class="font-weight-bold mb-1">
+        Password
+      </v-label>
+
+      <v-text-field
+        hide-details
+        type="password"
+        color="primary"
+        variant="outlined"
+      />
+    </v-col>
+
+    <v-col cols="12">
+      <v-btn
+        flat
+        block
+        to="/"
+        size="large"
+        color="primary"
+      >
+        Sign up
+      </v-btn>
+    </v-col>
+  </v-row>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineOptions({
+  name: 'RegisterForm',
+})
+
+const checkbox = ref(true)
+</script>
